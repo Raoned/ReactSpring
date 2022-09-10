e89b8780c977ef373009e658d287b99e0b052dbf
Raone Dourado <raoned99@gmail.com>
2022-09-10 14:37:54 -0400

    Send Notification

--- frontend/src/componentes/SalesCard/index.tsx
+++ frontend/src/componentes/SalesCard/index.tsx
@@ -17,11 +17,6 @@ function SalesCard() {
     useEffect(() => {
         const dmin = minDate.toISOString().slice(0, 10)
         const dmax = maxDate.toISOString().slice(0, 10)
-
-        console.log(dmin)
-
-
-
         axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then(response => { setSales(response.data.content) })
     }, [minDate, maxDate]);
 
@@ -73,7 +68,7 @@ function SalesCard() {
                                 <td>{sale.amount.toFixed(2)}</td>
                                 <td>
                                     <div className="dsmeta-red-btn-container">
-                                        <NotificationButton />
+                                        <NotificationButton saleId={sale.id} />
                                     </div>
 
 
